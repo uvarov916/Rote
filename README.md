9c388984e03783f224b812058889420fc02e1e4d
Ivan Uvarov <tonk2012@gmail.com>
2014-11-15 04:35:17 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Built by Ivan and Norway at Design&Hack.
---
+++ Rote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rote
+//
+//  Created by Ivan Uvarov on 11/15/14.
+//  Copyright (c) 2014 Rote Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
